f6b087fa533f6322b5c3bc9f6376e6671cbf7896
Mahdi Riahi <mahdiriahi972@yahoo.com>
2025-09-30 14:52:55 +0330

    create and implement app pages

---
+++ pages/views.py
@@ -0,0 +1,12 @@
+from django.views import generic
+
+
+class HomePageView(generic.TemplateView):
+    template_name = 'pages/home_page.html'
+
+class AboutPageView(generic.TemplateView):
+    template_name = 'pages/about_page.html'
+
+class ContactPageView(generic.TemplateView):
+    template_name = 'pages/contact_page.html'
+
